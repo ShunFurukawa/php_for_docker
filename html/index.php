4f9081c51d2d428e7ce065225b7df14068f5e694
Shun Furukawa <furukawa9312@gmail.com>
2018-12-03 14:32:40 +0900

    ADD sample form

--- html/index.php
+++ html/index.php
@@ -1,3 +1,5 @@
-<?php
-  phpinfo();
-?>
+<form action="action.php" method="post">
+  name:  <input type="text" name="name" />
+  age:   <input type="text" name="age" />
+  <input type="submit" />
+</form>
